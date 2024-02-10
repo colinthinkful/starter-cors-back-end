334cb272800443c9fd6d08fa10e800a150bf67ba
Colin Harrison <colinharrison@indeed.com>
2024-02-09 21:44:13 -0800

    Added comments to display various ways to implement CORS

--- src/cors-enabled/cors-enabled.router.js
+++ src/cors-enabled/cors-enabled.router.js
@@ -1,16 +1,28 @@
 const router = require("express").Router();
 const controller = require("./cors-enabled.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
+const cors = require("cors");
+
+// Enable CORS for this router
+// router.use(cors());
+
+// Enable CORS for specific request type
+// const corsGet = cors({ methods: "GET" });
 
 router
   .route("/:corsId")
   .get(controller.read)
+  // Adds CORS for specific request type
+  // .get(corsGet)
+  // .options(corsGet)
   .put(controller.update)
   .delete(controller.delete)
   .all(methodNotAllowed);
 
 router
   .route("/")
+  // Enables CORS for single request
+  //.get(cors(), controller.list)
   .get(controller.list)
   .post(controller.create)
   .all(methodNotAllowed);
